bcafad046d0673a3c9e78d3717fd35314ca47b85
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2018-12-27 07:44:43 +0100

    Add initial configuration files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Quick start repository
+
+This repository houses a simple set up that I am planning to use to build simple 
+desktop apps with elm, typescript, electron, python and any other languages.
+The idea is to separate the program into two parts. A GUI side, written in 
+elm, typescript and electron, which provides the view, and a kernel side, which
+does the proverbial heavy lifting. This kernel side could be implemented in
+python, or any other language with which there exists some interop with javascript.
+
+The main idea is to be able to use webdevelopment tools to build GUIs, while still
+keeping some of the performance of proper desktop applications. I do not have the 
+illusion that I am an experienced web developer, so use this set-up on your own risk.
+Chances are I made some big no-nos while setting up. I hope to resolve some of these
+no-nos in the feature. 
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const MODE =
+      process.env.npm_lifecycle_event === "build" ? "production" : "development";
+
+
+// Plugins of the main config shared between development and production
+var commonMainPlugins = [];
+var developmentMainPlugins = [ // Suggested for hot-loading.
+                               new webpack.NamedModulesPlugin() 
+                               // Prevents compilation errors causing the hot loader to 
+                               // lose state.
+                             , new webpack.NoEmitOnErrorsPlugin() 
+                             ];
+
+var productionMainPlugins = [];
+
+// Config for the main.js bundle, which contains all relevant code for the main process
+// of the electron app.
+var mainConfig = { mode: MODE
+                 , entry: { main: "./src/gui/main.ts" }
+                 , target: "electron-main"
+                 , output: { path: path.resolve(__dirname, "target")
+                           , filename: "[name].js"
+                           }
+                 , plugins: commonMainPlugins.concat( MODE === "development"
+                                                           ? developmentMainPlugins
+                                                           : productionMainPlugins)
+                 , module: { rules: [ { test: /\.ts$/, loader: "ts-loader" }
+                                    ]
+                           }
+                 , resolve: { extensions: [".js", ".ts", ]
+                            }
+                 , serve: { inline: true
+                          , stats: "errors-only"
+                          }
+                 };
+
+
+// Plugins of the renderer config shared between development and production
+var commonRendererPlugins = [ new HtmlWebpackPlugin({ title: "Quick-start application"
+                                                    , template: "./src/gui/index.html"
+                                                })
+                            ];
+
+var developmentRendererPlugins = [ // Suggested for hot-loading.
+                                   new webpack.NamedModulesPlugin() 
+                                   // Prevents compilation errors causing the hot loader to 
+                                   // lose state.
+                                 , new webpack.NoEmitOnErrorsPlugin() 
+                                 ];
+
+var productionRendererPlugins = [];
+
+
+//  Config for the renderer.js bundle, which contains all relevant code for the 
+// renderer process of the electron app.
+var rendererConfig = 
+        { mode: MODE
+        , entry: { renderer: "./src/gui/renderer.ts" }
+        , target: "electron-renderer"
+        , output: { path: path.resolve(__dirname, "target")
+                  , filename: "[name].js"
+                  }
+        , plugins: commonRendererPlugins.concat( MODE === "development"
+                                                      ? developmentRendererPlugins
+                                                      : productionRendererPlugins)
+        , module: { rules: [ { test: [/\.elm$/]
+                             , exclude: [/elm-stuff/, /node_modules/]
+                             , use: [ { loader: "elm-hot-webpack-loader" }
+                                    , { loader: "elm-webpack-loader"
+                                      , options: MODE === "production" 
+                                                      ? {} 
+                                                      : { debug: true
+                                                        , forceWatch: true 
+                                                        }
+                                      }
+                                    ]
+                             }
+                           , { test: /\.ts$/, loader: "ts-loader" }
+                           ]
+                  }
+        , resolve: { extensions: [".js", ".ts", ".elm"] }
+        , serve: { inline: true
+                 , stats: "errors-only"
+                 }
+        };
+
+
+module.exports = function(env) { 
+    return [ mainConfig, rendererConfig ];
+};
